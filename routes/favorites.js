6314089d1a4a3df12cf315328417be517c11f75b
Md Tahmid Bin Towhid <mdtahmidiiuc@gmail.com>
2025-11-28 12:00:45 +0600

    Update favorites.js

--- routes/favorites.js
+++ routes/favorites.js
@@ -2,36 +2,51 @@
 const express = require("express");
 const router = express.Router();
 const { ObjectId } = require("mongodb");
-const connectDB = require("../db");
+const { connectDB } = require("../db");
 
 // ADD TO FAVORITES
 router.post("/", async (req, res) => {
-  const db = await connectDB();
-  const favorites = db.collection("Favorites");
-
-  const result = await favorites.insertOne(req.body);
-  res.send(result);
+  try {
+    const db = await connectDB();
+    const favorites = db.collection("Favorites");
+    const result = await favorites.insertOne(req.body);
+    res.json(result);
+  } catch (err) {
+    console.error("Error POST /favorites:", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 // DELETE FAVORITE
 router.delete("/:id", async (req, res) => {
-  const db = await connectDB();
-  const favorites = db.collection("Favorites");
+  try {
+    const id = req.params.id;
+    if (!ObjectId.isValid(id)) return res.status(400).json({ message: "Invalid ID" });
 
-  const result = await favorites.deleteOne({ _id: new ObjectId(req.params.id) });
-  res.send(result);
+    const db = await connectDB();
+    const favorites = db.collection("Favorites");
+    const result = await favorites.deleteOne({ _id: new ObjectId(id) });
+    res.json(result);
+  } catch (err) {
+    console.error("Error DELETE /favorites/:id:", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
-// GET FAVORITES BY USER
-router.get("/", async (req, res) => {   // ← FIXED
-  const db = await connectDB();
-  const favorites = db.collection("Favorites");
+// GET FAVORITES BY USER (query ?email=...)
+router.get("/", async (req, res) => {
+  try {
+    const email = req.query.email;
+    if (!email) return res.status(400).json({ message: "Email query missing" });
 
-  const email = req.query.email;
-
-  const result = await favorites.find({ user_email: email }).toArray();
-
-  res.send(result);
+    const db = await connectDB();
+    const favorites = db.collection("Favorites");
+    const result = await favorites.find({ user_email: email }).toArray();
+    res.json(result);
+  } catch (err) {
+    console.error("Error GET /favorites:", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
